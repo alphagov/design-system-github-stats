501a6377127773c536d85356fa82d31a8c5b13e6
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2024-12-18 15:57:55 +0000

    DO NOT MERGE: Limit dataset for testing

--- build-filtered-data.mjs
+++ build-filtered-data.mjs
@@ -1,12 +1,12 @@
 import { writeFileSync } from 'fs'
 
 import { Octokit, RequestError } from 'octokit'
-import { throttling } from '@octokit/plugin-throttling';
+import { throttling } from '@octokit/plugin-throttling'
 import * as yarnLock from '@yarnpkg/lockfile'
 import checkDenyList from './helpers/check-deny-list.mjs'
 import checkServiceOwner from './helpers/check-service-owner.mjs'
 
-import rawDeps from './data/raw-deps.json' assert {type: 'json'}
+import rawDeps from './data/raw-deps.json' assert { type: 'json' }
 
 const MyOctokit = Octokit.plugin(throttling)
 const octokit = new MyOctokit({
@@ -14,22 +14,22 @@ const octokit = new MyOctokit({
   throttle: {
     onRateLimit: (retryAfter, options, octokit, retryCount) => {
       octokit.log.warn(
-        `Request quota exhausted for request ${options.method} ${options.url}`,
-      );
+        `Request quota exhausted for request ${options.method} ${options.url}`
+      )
 
       if (retryCount < 1) {
         // only retries once
-        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
-        return true;
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+        return true
       }
     },
     onSecondaryRateLimit: (retryAfter, options, octokit) => {
       // does not retry, only logs a warning
       octokit.log.warn(
-        `SecondaryRateLimit detected for request ${options.method} ${options.url}`,
-      );
+        `SecondaryRateLimit detected for request ${options.method} ${options.url}`
+      )
     },
-  }
+  },
 })
 
 class NoPackageJsonError extends Error {}
@@ -42,7 +42,7 @@ async function filterDeps() {
   const builtData = []
 
   console.log('Analysis BEGIN')
-  for (const repo of rawDeps.all_public_dependent_repos) {
+  for (const repo of rawDeps.all_public_dependent_repos.slice(0, 100)) {
     const repoOwner = repo.owner
     const repoName = repo.repo_name
     let builtByGovernment = false
@@ -57,7 +57,9 @@ async function filterDeps() {
       console.log(`Analysing ${repo.name}...`)
 
       if (checkDenyList(repoName, repoOwner)) {
-        console.log(`${repo.name} is on the 'deny' list and will not be processed`)
+        console.log(
+          `${repo.name} is on the 'deny' list and will not be processed`
+        )
         continue
       }
 
@@ -66,7 +68,9 @@ async function filterDeps() {
       if (builtByGovernment) {
         console.log(`${repo.name} looks like a GOV.UK service.`)
       } else {
-        console.log(`${repo.name} looks like it ISN'T a GOV.UK service. This has been noted.`)
+        console.log(
+          `${repo.name} looks like it ISN'T a GOV.UK service. This has been noted.`
+        )
       }
 
       console.log('Getting repo data...')
@@ -74,23 +78,23 @@ async function filterDeps() {
       const firstCommit = await octokit.rest.repos.listCommits({
         owner: repoOwner,
         repo: repoName,
-        per_page: 1
+        per_page: 1,
       })
 
       const repoTree = await octokit.rest.git.getTree({
         owner: repoOwner,
         repo: repoName,
         tree_sha: firstCommit.data[0].sha,
-        recursive: true
+        recursive: true,
       })
 
-      if (!repoTree.data.tree.find(file => file.path == 'package.json')) {
-        throw new NoPackageJsonError
+      if (!repoTree.data.tree.find((file) => file.path == 'package.json')) {
+        throw new NoPackageJsonError()
       }
 
-      if (repoTree.data.tree.find(file => file.path == 'package-lock.json')) {
+      if (repoTree.data.tree.find((file) => file.path == 'package-lock.json')) {
         lockfileType = 'package-lock.json'
-      } else if (repoTree.data.tree.find(file => file.path == 'yarn.lock')) {
+      } else if (repoTree.data.tree.find((file) => file.path == 'yarn.lock')) {
         lockfileType = 'yarn.lock'
       }
 
@@ -99,31 +103,42 @@ async function filterDeps() {
         repo: repoName,
         path: 'package.json',
         headers: {
-          accept: 'application/vnd.github.raw+json'
-        }
+          accept: 'application/vnd.github.raw+json',
+        },
       })
 
       const packageObject = JSON.parse(packageFile.data)
-      
+
       if (!('dependencies' in packageObject)) {
-        throw new CouldntReadPackageError
+        throw new CouldntReadPackageError()
       }
 
-      isPrototype = (repoTree.data.tree.find(file => file.path == 'lib/usage_data.js') != undefined) || ('govuk-prototype-kit' in packageObject.dependencies)
+      isPrototype =
+        repoTree.data.tree.find((file) => file.path == 'lib/usage_data.js') !=
+          undefined || 'govuk-prototype-kit' in packageObject.dependencies
 
       if (isPrototype) {
-        console.log(`${repo.name} looks like an instance of the prototype kit. This has been noted.`)
+        console.log(
+          `${repo.name} looks like an instance of the prototype kit. This has been noted.`
+        )
       }
-      
+
       if (!('govuk-frontend' in packageObject.dependencies)) {
-        throw new IndirectDependencyError
+        throw new IndirectDependencyError()
       }
 
       frontendVersion = packageObject.dependencies['govuk-frontend']
-      console.log(`${repo.name} is using GOV.UK Frontend version ${frontendVersion}`)
+      console.log(
+        `${repo.name} is using GOV.UK Frontend version ${frontendVersion}`
+      )
 
-      if (frontendVersion.indexOf('^') != -1 || frontendVersion.indexOf('~') != -1) {
-        console.log(`${repo.name} is using approximation syntax in their GOV.UK Frontend version declaration, meaning their actual version might be different to what's in their package.json. Checking their lockfile...`)
+      if (
+        frontendVersion.indexOf('^') != -1 ||
+        frontendVersion.indexOf('~') != -1
+      ) {
+        console.log(
+          `${repo.name} is using approximation syntax in their GOV.UK Frontend version declaration, meaning their actual version might be different to what's in their package.json. Checking their lockfile...`
+        )
 
         if (lockfileType == 'package-lock.json') {
           const packageLockFile = await octokit.rest.repos.getContent({
@@ -131,16 +146,20 @@ async function filterDeps() {
             repo: repoName,
             path: 'package-lock.json',
             headers: {
-              accept: 'application/vnd.github.raw+json'
-            }
+              accept: 'application/vnd.github.raw+json',
+            },
           })
 
           try {
             const packageLockObject = JSON.parse(packageLockFile.data)
             if ('packages' in packageLockObject) {
-              frontendVersion = packageLockObject.packages['node_modules/govuk-frontend']?.version || frontendVersion
+              frontendVersion =
+                packageLockObject.packages['node_modules/govuk-frontend']
+                  ?.version || frontendVersion
             } else if ('dependencies' in packageLockObject) {
-              frontendVersion = packageLockObject.dependencies['govuk-frontend']?.version || frontendVersion
+              frontendVersion =
+                packageLockObject.dependencies['govuk-frontend']?.version ||
+                frontendVersion
             }
           } catch (e) {
             console.log('There was a problem with processing this lockfile:', e)
@@ -151,20 +170,27 @@ async function filterDeps() {
             repo: repoName,
             path: 'yarn.lock',
             headers: {
-              accept: 'application/vnd.github.raw+json'
-            }
+              accept: 'application/vnd.github.raw+json',
+            },
           })
 
           try {
             const yarnLockObject = yarnLock.default.parse(yarnLockFile.data)
-            frontendVersion = yarnLockObject.object[`govuk-frontend@${frontendVersion}`]?.version || frontendVersion
+            frontendVersion =
+              yarnLockObject.object[`govuk-frontend@${frontendVersion}`]
+                ?.version || frontendVersion
           } catch (e) {
             console.log('There was a problem with processing this lockfile:', e)
           }
         }
 
-        if (frontendVersion.indexOf('^') != -1 || frontendVersion.indexOf('~') != -1) {
-          console.log(`Something went wrong in lockfile processing so we'll have to assume GOV.UK Frontend version for now.`)
+        if (
+          frontendVersion.indexOf('^') != -1 ||
+          frontendVersion.indexOf('~') != -1
+        ) {
+          console.log(
+            `Something went wrong in lockfile processing so we'll have to assume GOV.UK Frontend version for now.`
+          )
           frontendVersion = frontendVersion.replace('^', '').replace('~', '')
           versionDoubt = true
         }
@@ -191,7 +217,7 @@ async function filterDeps() {
         couldntAccess = true
         versionDoubt = true
       } else {
-        throw (e)
+        throw e
       }
     }
 
@@ -203,16 +229,25 @@ async function filterDeps() {
       versionDoubt,
       builtByGovernment,
       indirectDependency,
-      isPrototype
+      isPrototype,
     })
 
     console.log(`Analysis of ${repo.name} complete`)
 
-    await writeFileSync('data/filtered-data.json', JSON.stringify(builtData, null, 2))
+    await writeFileSync(
+      'data/filtered-data.json',
+      JSON.stringify(builtData, null, 2)
+    )
     console.log('Data updated')
 
-    const index = rawDeps.all_public_dependent_repos.findIndex(item => item === repo)
-    console.log(`This was repo number ${index + 1} of ${rawDeps.all_public_dependent_repos.length}`)
+    const index = rawDeps.all_public_dependent_repos.findIndex(
+      (item) => item === repo
+    )
+    console.log(
+      `This was repo number ${index + 1} of ${
+        rawDeps.all_public_dependent_repos.length
+      }`
+    )
 
     const rateLimit = await octokit.rest.rateLimit.get()
     console.log(`${rateLimit.data.rate.remaining} remaining on rate limit`)

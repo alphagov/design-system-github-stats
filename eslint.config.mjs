7c0b34b34b72325a6d14e602fa6afaa347dac0f0
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2025-01-03 09:25:18 +0000

    Add neostandard for lint config
    If you're running Prettier or somesuch on your IDE, you'll want to make sure that at workspace level you're using eslint instead.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,22 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
+import neostandard from 'neostandard'
 import babelParser from '@babel/eslint-parser'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  ...neostandard({}),
   {
     languageOptions: {
-      globals: globals.node,
+      // We need Babel to parse the import assertions.
+      // This will likely change since these are stage 4 proposals, and neostandard
+      // should soon support them natively.
       parser: babelParser,
       parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          plugins: ['@babel/plugin-syntax-import-assertions'],
-        },
-      },
+        requireConfigFile: false
+      }
     },
-  },
-  pluginJs.configs.recommended,
+    rules: {
+      // neostandard relaxes the comma-dangle rule, but we prefer not to
+      '@stylistic/comma-dangle': ['error', 'never']
+    }
+  }
 ]

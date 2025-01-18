4a8ccfaf8e6b3dd3853feea3a6e02307924641b9
Brett Kyle <brett.kyle@digital.cabinet-office.gov.uk>
2025-01-18 00:10:07 +0000

    Update build script to add keyData

--- build-filtered-data.mjs
+++ build-filtered-data.mjs
@@ -95,6 +95,58 @@ async function filterDeps () {
     `data/${yyyymmdd}-${timestamp}-unprocessedItems.json`,
     JSON.stringify(unprocessedItems, null, 2)
   )
+
+  // Extract key data
+  const keyData = {
+    date: new Date(),
+    total: builtData.length,
+    totalServices: builtData.filter(result => result.service).length,
+    top75: builtData.filter(result => result.service?.top75).length,
+    version0: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('0.')
+    }).length,
+    version1: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('1.')
+    }).length,
+    version2: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('2.')
+    }).length,
+    version3: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('3.')
+    }).length,
+    version4: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('4.')
+    }).length,
+    version5: builtData.filter(result => {
+      const dependencies = result.isIndirect ? result.indirectDependencies : result.directDependencies
+      return dependencies?.[0]?.actualVersion.startsWith('5.')
+    }).length,
+    totalGovernment: builtData.filter(result => result.builtByGovernment).length,
+    totalPrototypes: builtData.filter(result => result.isPrototype).length,
+    unknownLockfile: builtData.filter(result => result.unknownLockFileType).length,
+    activeRepos: builtData.filter(result => {
+      const updatedAt = new Date(result.updatedAt)
+      const oneYearAgo = new Date()
+      oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+      return updatedAt > oneYearAgo
+    }).length,
+    activeGovRepos: builtData.filter(result => {
+      const updatedAt = new Date(result.updatedAt)
+      const oneYearAgo = new Date()
+      oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+      return result.builtByGovernment && updatedAt > oneYearAgo
+    }).length
+  }
+
+  await appendFileSync(
+    'data/key-data.json',
+    JSON.stringify(keyData, null, 2)
+  )
   console.log("We're done!")
 }
 
